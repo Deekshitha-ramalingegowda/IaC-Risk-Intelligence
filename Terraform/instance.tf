3d0fee8862f7437786bb1dbcf4f4a99b2c722616
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-03-04 15:21:34 +0530

    change22

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -4,7 +4,7 @@
     instance_type = "t2.micro"
     subnet_id              = aws_subnet.public_subnet.id
     vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-    ebs_optimized = true
+    ebs_optimized = false
 
     tags = {
         Name = "WebServer"
