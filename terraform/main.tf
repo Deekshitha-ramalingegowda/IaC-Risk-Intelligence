94fed19ec4e13fd38ffa4b779e572532a664e309
ArunimaDas-3 <arunima.das@devon.nl>
2026-03-12 14:53:24 +0530

    changes added

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "bad_sg" {
 
   ingress {
     from_port   = 22
-    to_port     = ""  ##changes made 
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # CRITICAL: Unrestricted SSH
   }
