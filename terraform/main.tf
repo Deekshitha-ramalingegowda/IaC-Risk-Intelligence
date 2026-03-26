6034661b14ea6aa71bb41b1c72c471bc0f9bb6ff
Deekshitha Ramalingegowda (DevOn) <d.ramalingegowda@devon.nl>
2026-03-26 12:31:38 +0530

    infracost

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,14 +4,13 @@ provider "aws" {
  
 resource "aws_instance" "app" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "m5.2xlarge"
+  instance_type = "t3.large"
   monitoring    = false
   root_block_device {
     volume_type = "gp2"
     volume_size = 500
     encrypted   = false
   }
- 
   tags = {
     Name = "app-server"
   }
