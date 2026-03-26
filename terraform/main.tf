1382660d329c93ed8d49f985a1a79f405ed66ebf
ArunimaDas-3 <arunima.das@devon.nl>
2026-03-26 17:26:49 +0530

    s3-added

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,31 @@ resource "aws_instance" "app" {
   }
 }
  
+
+ resource "aws_s3_bucket" "bad_bucket" {
+  bucket = "my-insecure-bucket-12345"
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "bad_block" {
+  bucket = aws_s3_bucket.bad_bucket.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bad_acl" {
+  bucket = aws_s3_bucket.bad_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_versioning" "bad_versioning" {
+  bucket = aws_s3_bucket.bad_bucket.id
+
+  versioning_configuration {
+    status = "Suspended"
+  }
+}
